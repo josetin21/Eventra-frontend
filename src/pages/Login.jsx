d02b9a5e4adc8b9b43f6b8d20900795bef2c4ed4
josetin21 <josetin2001@gmail.com>
2026-03-21 01:47:03 +0530

    feat: add Login page with JWT auth and role-based redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,99 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from '../context/AuthContext'
+import api from '../api/axios'
+
 export default function Login(){
-    return <div>Login Pge</div>
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const { login } = useAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        setError('')
+        setLoading(true)
+
+        try{
+            const response = await api.post('/auth/login', { email, password })
+            login(response.data)
+
+            const role = response.data.role
+            if(role === 'ADMIN') navigate('/admin-dashboard')
+            else if(role === 'ORGANIZER') navigate('/organizer-dashboard')
+            else navigate('/')
+        } catch(err){
+            setError("Invalid email or password")
+        } finally{
+            setLoading(false)
+        }
+    }
+
+    return(
+        <div className="min-h-screen bg-grey-50 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+
+                <h1 className="text-2xl font-bold text-center text-blue-600 mb-2">
+                    Welcome to Eventra
+                </h1>
+                <p className="text-center text-gray-500 mb-6">Sign in to your account</p>
+
+                {error &&(
+                    <div className="bg-red-50 text-red-600 p-3 rounded mb-4 text-sm">
+                        {Error}
+                    </div>
+                )}
+
+                <form onSubmit={handleSubmit}>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full border-gray-300 rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
+                            placeholder="you@example.com"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-6">
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            placeholder="••••••••"
+                            required
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="w-full bg-blue-600 text-white py-2 rounded font-medium hover:bg-blue-700 disabled:opacity-50"
+                    >
+                        {loading ? 'Signing in....' : 'Sign In'}
+                    </button>
+                </form>
+
+                <p className="text-center text-sm text-gray-500 mt-4">
+                    Don't have an account?{' '}
+                    <Link to="/register" className="text-blue-600 hover:underline">
+                        Register here
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
 }
