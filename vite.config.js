b80842d49a31817f60662d534a1f8964f59e20de
josetin21 <josetin2001@gmail.com>
2026-04-15 09:58:26 +0530

    feat: complete Eventra project and prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'node:fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+        host: true,
+        port: 5173,
+        https: {
+            key: fs.readFileSync("certs/172.20.10.2+2-key.pem"),
+            cert: fs.readFileSync("certs/172.20.10.2+2.pem"),
+        },
+    },
 })
